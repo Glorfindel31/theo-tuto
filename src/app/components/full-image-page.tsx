c966987a156871ef6fe5ac542e35b00dd65a1cfb
glorfindel31 <cedric31flo@gmail.com>
2024-04-19 15:21:33 +0700

    small css changes

--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -4,14 +4,15 @@ export default async function FullPageImageView(props: { id: number }) {
   const image = await getImageById(props.id);
   return (
     <div className="flex h-full w-full min-w-0">
-      <div className="flex-shrink"></div>
-      <img
-        src={image.url}
-        alt={image.name}
-        className="flex-shrink object-contain"
-      />
+      <div className="flex flex-shrink justify-center">
+        <img
+          src={image.url}
+          alt={image.name}
+          className="flex-shrink object-contain"
+        />
+      </div>
       ;
-      <div className="flex w-48 flex-shrink-0 flex-col">
+      <div className="flex w-72 flex-shrink-0 flex-col border-l">
         <div className="text-xl font-semibold">{image.name}</div>
         <div className="text-xl font-semibold">{image.url}</div>
       </div>
